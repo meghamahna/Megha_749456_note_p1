db1338f0e2bd5747c3de6c61078b8dfb565a9779
Megha Mahna <meghamahna04@gmail.com>
2019-11-09 13:24:35 -0500

    Initial Commit

---
+++ Megha_749456_note_p1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Megha_749456_note_p1
+//
+//  Created by Megha Mahna on 2019-11-09.
+//  Copyright © 2019 Megha Mahna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
